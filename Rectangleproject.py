9c08d0c0c051d8314fcdb9c9ce943deb56e17c7e
Hello-star19 <mirza.mahid@gmail.com>
2025-06-28 17:22:21 +0100

    Add files via upload

--- Rectangleproject.py
+++ Rectangleproject.py
@@ -13,8 +13,18 @@ pygame.display.update()
 Run = True
 
 #creating a class for the rectangle
-class rectangle():
+class rectangles():
     def __init__(self, color, size):
+        self.surface = bg
+        self.color = color
+        self.size = size
+    # Draw function
+    def draw(self):
+        self.rectangle = pygame.draw.rect(self.surface, self.color, self.size)
+
+rectangle1 = rectangles(c2, (10, 10, 50, 60))
+rectangle2 = rectangles(c4, (80, 90, 100, 140))
+rectangle3 = rectangles(c1, (340,390, 50, 100))
 
 
 
@@ -25,6 +35,11 @@ while Run:
     for event in pygame.event.get():   # Fetches all events happening
         if event.type == pygame.QUIT:
             Run = False
+    rectangle1.draw()
+    rectangle2.draw()
+    rectangle3.draw()
+    pygame.display.update()
+
 
 pygame.quit()
         
