e6b29e2155feba06bf5bdd64d1f3fb016e35ff73
Hello-star19 <mirza.mahid@gmail.com>
2025-10-14 16:59:52 +0100

    recycle.py

--- Recycling game/recycle.py
+++ Recycling game/recycle.py
@@ -4,9 +4,20 @@ import time
 
 from pygame.locals import* 
 
+
 pygame.init()
 
+background = pygame.image.load("background.jpg")
+background = pygame.transform.scale(background,(1000,700))
 bg = pygame.display.set_mode((1000,700))
+# gaming variables
+score = 0
+start_time = time.time()
+font_style = pygame.font.SysFont("Arial",20)
+
+
+
+
 
 # class for recyclable items
 class recycle(pygame.sprite.Sprite):
@@ -25,6 +36,35 @@ for i in range(40):
 
 
 
+# class for recyclable items
+class non_recycle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("plastic.png")
+        self.image = pygame.transform.scale(self.image,(40,40))
+        self.rect = self.image.get_rect()
+nrgroup = pygame.sprite.Group()
+for i in range(30):
+    item = non_recycle() 
+    item.rect.x = random.randint(20,940)
+    item.rect.y = random.randint(20,640)
+    nrgroup.add(item)
+
+
+class bin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("bin.png")
+        self.image = pygame.transform.scale(self.image,(40,40))
+        self.rect = self.image.get_rect()
+bingroup = pygame.sprite.Group()
+for i in range(1):
+    bins = bin() 
+    bins.rect.x = (10)
+    bins.rect.y = (10)
+    bingroup.add(bins)
+
+
 
 
 
@@ -33,8 +73,34 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    rgroup.draw(bg)
+    # total time
+    total_time = time.time()- start_time
+    if total_time > 20:
+        message = font_style.render("Game over",True,"Blue")
+    else: 
+        bg.blit(background,(0,0))
+        timer = font_style.render(str(20-int(total_time)), True,"White")
+        bg.blit(timer,(100,0))
+        rgroup.draw(bg)
+        nrgroup.draw(bg)
+        bingroup.draw(bg)
+        # Controlling the bin
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            bins.rect.y-= 1
+        if keys[pygame.K_DOWN]:
+            bins.rect.y+= 1
+        if keys[pygame.K_RIGHT]:
+            bins.rect.x+= 1
+        if keys[pygame.K_LEFT]:
+            bins.rect.x-= 1
+
+   
+
     pygame.display.update()
 pygame.quit()
+
+
+
 
 
